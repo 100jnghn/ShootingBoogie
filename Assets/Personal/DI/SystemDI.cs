7b96d0873cc0ceebab1c708c0e2c332a4e61fe7c
dain0l <dlekdls100@naver.com>
2024-12-03 17:45:10 +0900

    Update SystemDI.cs

--- Assets/Personal/DI/SystemDI.cs
+++ Assets/Personal/DI/SystemDI.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 관련
 
 public class SystemDI : MonoBehaviour
 {
     public Transform firePos;   // 투사체 생성 위치
     public GameObject bullet;    // 공격시 생성하는 투사체
     public AudioSource sfxFire;  // 공격 소리 이펙트
-
+    public Animator animator;     // 공격 애니메이션
+    public Image cooldownImage;   // 쿨다운 UI 이미지
 
     [Header("----- Attack -----")]
     public float attackTime = 1f;
@@ -17,7 +19,10 @@ public class SystemDI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cooldownImage != null)
+        {
+            cooldownImage.fillAmount = 1; // 초기화
+        }
     }
 
     // Update is called once per frame
@@ -43,6 +48,12 @@ public class SystemDI : MonoBehaviour
             {
                 isAttackable = true;
             }
+
+            // 쿨다운 UI 업데이트
+            if (cooldownImage != null)
+            {
+                cooldownImage.fillAmount = 1 - (currentTime / attackTime);
+            }
         }
     }
 
@@ -56,7 +67,8 @@ public class SystemDI : MonoBehaviour
 
         Instantiate(bullet, firePos.position, firePos.rotation);   // bullet 생성
         sfxFire.Play();       // sound 재생
-        
+        animator.SetTrigger("Attack"); // 애니메이션 재생 (애니메이터 설정 필요)
+
         currentTime = 0;
         isAttackable = false;
     }
